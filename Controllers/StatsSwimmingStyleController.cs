baa2241c36605d945eb57f261d36ff299347861d
Ouzinem <ouzinem@hotmail.com>
2023-06-08 11:25:08 -0400

    Validation using Data Annotations

--- Controllers/StatsSwimmingStyleController.cs
+++ Controllers/StatsSwimmingStyleController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using SwimmingStyleAPI.Models.StatsDto;
 using SwimmingStyleAPI.Models.SwimmingStyleDto;
+using System.Net.NetworkInformation;
 
 namespace SwimmingStyleAPI.Controllers
 {
@@ -53,14 +54,23 @@ namespace SwimmingStyleAPI.Controllers
                 return NotFound();
             }
 
-            // demo purposes - to be improved 
             // need to calculate the ide of the new item 
             var maxStatsId = SwimmingStyleDataStore.Current.SwimmingStyles.SelectMany(
                                c => c.StatsOfSwimmingStyle).Max(p => p.IdStats);
+            if (maxStatsId == 0)
+            {
+                maxStatsId = 1;
+            }
+            else
+            {
+                maxStatsId++;
+            }
+
             // need to mapping because we work with swimming style dto and i need to create stats dto
             var finalStatsSwimmingStyle = new StatsSwimmingstyleDto()
             {
-                IdStats = ++maxStatsId,
+
+                IdStats = maxStatsId,
                 Speed = statsSwimmingStyle.Speed,
                 Endurance = statsSwimmingStyle.Endurance,
                 Technique = statsSwimmingStyle.Technique,
@@ -79,7 +89,7 @@ namespace SwimmingStyleAPI.Controllers
             //nameof(GetswimmingstyleById)
         }
 
-       
+
 
     }
 }
