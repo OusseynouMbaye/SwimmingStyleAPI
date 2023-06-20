f45171bf325c6d8b18748bbb1c9fda5286bdbb2e
Ouzinem <ouzinem@hotmail.com>
2023-06-19 20:19:54 -0400

    Using repositiory  for swimming style controller

---
+++ Models/SwimmingStyleDto/SwimmingStyleWithoutStatsOfSwimmingStyleDto.cs
@@ -0,0 +1,10 @@
+﻿namespace SwimmingStyleAPI.Models.SwimmingStyleDto
+{
+    public class SwimmingStyleWithoutStatsOfSwimmingStyleDto
+    {
+        public int SwimmingStyleId { get; set; }
+        public string Name { get; set; } = String.Empty;
+        public string? Description { get; set; }
+        //public string? Tags { get; set; }
+    }
+}
